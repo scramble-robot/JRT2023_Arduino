2b451bf9a3d094b47e627e81f2c85b6673e8349a
Gasuta722 <dqmj.y.s@gmail.com>
2023-08-20 10:08:06 +0900

    JRT用に足回り，射出，ピッチの部分を削除

--- JRTsample/process.ino
+++ JRTsample/process.ino
@@ -1,38 +1,3 @@
-void Wheel(void){
-  //不感帯はif文の条件の数値
-  //斜め移動のときの横成分を増やしたいときはRxData[0]を割っている4を変える
-  //全体の速度を変えたいときは1.3の部分を変える
-  //アナログスティック2本になるともっと簡単になる
-  if((RxData[1] > 10) ){
-    MotorON(WHEEL_L, (int)((-RxData[1] - RxData[0]/4)/1.3));
-    MotorON(WHEEL_R, (int)((-RxData[1] + RxData[0]/4)/1.3));
-  }else if((RxData[1] < -10) ){
-    MotorON(WHEEL_L, (int)((-RxData[1] + RxData[0]/4)/1.3));
-    MotorON(WHEEL_R, (int)((-RxData[1] - RxData[0]/4)/1.3));
-  }else if(((RxData[1] < 10) && (RxData[1] > -10)) || (RxData[0] > 10) || (RxData[0] < -10)){
-    MotorON(WHEEL_L, (int)(-RxData[0]/2));
-    MotorON(WHEEL_R, (int)(RxData[0]/2));
-  }  else{
-    MotorOFF(WHEEL_L);
-    MotorOFF(WHEEL_R);
-  }
-}
-
-void Shot(void){
-  //この関数の中を消してJRTで記載してもらうとシーケンス制御のお勉強ができる
-  if((RxData[3] & 0b100) && (ShotSeq == 0)){
-    MotorON(SHOT, 100);
-    ShotSeq = 1;
-  }else if((ShotSeq == 1) && (SHOT_FRONT_SW == 0)){
-    MotorON(SHOT, -100);
-    ShotSeq = 2;
-  }else if((ShotSeq == 2) && (SHOT_BACK_SW == 0)){
-    MotorOFF(SHOT);
-    ShotSeq = 0;
-  }
-  // Serial.println(ShotSeq);
-}
-
 void Roller(void){
   if((RxData[3] & 0b010) && (RollerOnOff == 0) && (RollerSeq == 0)){
     RollerSeq = 1;
@@ -52,9 +17,4 @@ void Roller(void){
     RollerSeq = 0;
     delay(100);
   }
-}
-
-void Pitch(void){
-  //サーボのお勉強がてらここも消すかも
-  ServoON(SERVO1, (int)(RxData[2]*90/100 + 90));
 }
---
+++ JRTsample/task.ino
@@ -0,0 +1,14 @@
+// 足回りの制御
+void Wheel(void){
+
+}
+
+// 射出のシーケンス
+void Shot(void){
+
+}
+
+// 射出の角度調整
+void Pitch(void){
+
+}
